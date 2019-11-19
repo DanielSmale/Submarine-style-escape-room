2b93b1334ab4f6d66260ecca6ca9c2c6d044da9f
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2019-11-19 18:44:05 +0000

    Main room in unity

---
+++ AINT 253 - escape room/Assets/CharacterLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLook : MonoBehaviour
+{
+    // code referenced: https://www.youtube.com/watch?v=_QajrabyTJc
+
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+      
+    float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
